b73a9e0496c9f101ba8c7e9739ce8d5bd8267a8e
Niklas Farke <43673607+nfarke@users.noreply.github.com>
2023-03-07 13:05:05 +0100

    Update main.m

--- upshift/main.m
+++ upshift/main.m
@@ -4,7 +4,7 @@ config = dec2bin(0:2^N-1)' - '0';
 config = config';
 %parpool(17)
 for k = 1:length(config)
-    M = create_SS_solutions_function2(config(k,:),num2str(k));
+    M = create_SS_solutions_function(config(k,:),num2str(k));
 end
 
 for k = 1:length(config)
